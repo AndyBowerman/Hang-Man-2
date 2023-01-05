7523a70cfda2f52c969d5812cd53b81ad251e6ac
AndyBowerman <andy_bowerman06@hotmail.com>
2023-01-05 08:14:36 +0000

    removed unneeded comments

--- src/main/java/org/example/HangMan.java
+++ src/main/java/org/example/HangMan.java
@@ -1,23 +1,5 @@
 package org.example;
 
-/*
-text file storing possible words
-
-Classes
-
-generate word - store word and hidden word and reveal correct guesses
-guesses - store previous guesses
-run game - handles souting everything
-
-User interactions
-select difficulty - easy medium hard
-view word as ____
-make guess
-told if correct or incorrect
-shown guesses made & word with correct guesses unveiled
-shown hangman image
- */
-
 public class HangMan {
     public static void main(String[] args) {
         RunGame runGame = new RunGame();
